f4b932d56f9844d942c02c2b4a48e92b9f0d207b
Midlaj <midlajcv810@gmail.com>
2026-01-10 08:34:19 +0530

    homepage react

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import './assets/css/style.css'
+import { Header } from './components/header'
+import { Main } from './components/Main'
+import { Footer } from './components/footer'
 
 function App() {
 
   return (
     <>
-    
+     <Header />
+     <Main />
+     <Footer />
     </>
   )
 }
---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+   <>
+        <footer className='footer pt-3 mt-3'>
+            <hr className='border-bottom'/>
+            <p className='text-light text-center '>&copy; 2026 - Built with ❤ by Midlaj</p>
+        </footer>
+   </>
+  )
+}
